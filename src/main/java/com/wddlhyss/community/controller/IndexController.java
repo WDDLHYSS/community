625cf7881d7cc96c3a35ffe52f4db8c5971e1d81
wddlhyss <1076596573@qq.com>
2019-11-11 20:59:44 +0800

    add index andbootstrap

--- src/main/java/com/wddlhyss/community/controller/IndexController.java
+++ src/main/java/com/wddlhyss/community/controller/IndexController.java
@@ -4,15 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 
-public class HelloController {
+public class IndexController {
 
-    @RequestMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name , Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @RequestMapping("/")
+    public String hello(){
+        return "index";
     }
 }
